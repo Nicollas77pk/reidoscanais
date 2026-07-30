6c633ef878257966939c910715b4c7abbc1a1a16
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 10:22:30 -0300

    Update card.js

--- js/card.js
+++ js/card.js
@@ -2,39 +2,46 @@ function criarCard(canal){
 
     return `
 
-        <div class="card">
+    <div class="card" data-id="${canal.id}">
 
-            <div class="card-logo">
+        <div class="card-logo">
 
-                <img src="${canal.logo}" alt="${canal.nome}">
-
-            </div>
-
-            <div class="card-info">
-
-                <h3>${canal.nome}</h3>
-
-                <span class="status">
-
-                    AO VIVO
-
-                </span>
-
-                <p class="programa">
-
-                    ${canal.programa || "Programação ao vivo"}
-
-                </p>
-
-                <button class="assistir">
-
-                    ▶ Assistir
-
-                </button>
-
-            </div>
+            <img
+                src="${canal.logo_url}"
+                alt="${canal.name}"
+                loading="lazy">
 
         </div>
+
+        <div class="card-info">
+
+            <h3>
+
+                ${canal.name}
+
+            </h3>
+
+            <span class="status">
+
+                AO VIVO
+
+            </span>
+
+            <p class="programa">
+
+                ${canal.epg?.current?.title || "Programação indisponível"}
+
+            </p>
+
+            <button class="assistir">
+
+                ▶ Assistir
+
+            </button>
+
+        </div>
+
+    </div>
 
     `;
 
