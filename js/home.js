d5370d2ec6b542ccde61e87b1d4a144e9e64054e
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 10:12:50 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    console.log("Rei Dos Canais iniciado.");
+
+    const canais = await api("/channels");
+
+    console.log(canais);
+
+});
