ae8485fb3d99e4e4697e98385557e9e2b8ad41d6
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 10:12:09 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,25 @@
+async function api(endpoint) {
 
+    try {
+
+        const resposta = await fetch(CONFIG.API_URL + endpoint);
+
+        if (!resposta.ok) {
+
+            throw new Error("Erro ao conectar com a API.");
+
+        }
+
+        return await resposta.json();
+
+    }
+
+    catch (erro) {
+
+        console.error(erro);
+
+        return null;
+
+    }
+
+}
