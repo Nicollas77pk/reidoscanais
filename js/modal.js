b2dcbb3f4221f96398dc0f53874ac4cfcf9abec6
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 10:37:04 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,57 @@
+const modal = document.getElementById("playerModal");
 
+const frame = document.getElementById("playerFrame");
+
+const fechar = document.getElementById("fecharModal");
+
+const logo = document.getElementById("modalLogo");
+
+const titulo = document.getElementById("modalTitulo");
+
+const categoria = document.getElementById("modalCategoria");
+
+const atual = document.getElementById("programaAtual");
+
+const proximo = document.getElementById("proximoPrograma");
+
+function abrirPlayer(canal){
+
+    frame.src = canal.embeds[0].embed_url;
+
+    logo.src = canal.logo_url;
+
+    titulo.textContent = canal.name;
+
+    categoria.textContent = canal.category;
+
+    atual.textContent =
+        canal.epg?.current?.title || "-";
+
+    proximo.textContent =
+        canal.epg?.next?.title || "-";
+
+    modal.classList.add("ativo");
+
+    document.body.style.overflow="hidden";
+
+}
+
+fechar.onclick=()=>{
+
+    modal.classList.remove("ativo");
+
+    frame.src="";
+
+    document.body.style.overflow="auto";
+
+};
+
+modal.onclick=(e)=>{
+
+    if(e.target===modal){
+
+        fechar.click();
+
+    }
+
+};
