d89eceadbb7208c315075092643673daa52ed4a1
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 20:46:30 -0300

    Update canais.js

--- js/canais.js
+++ js/canais.js
@@ -1,1 +1,49 @@
+function montarCanaisDestaque(canais){
 
+    const slider = document.getElementById("canaisSlider");
+
+    if(!slider) return;
+
+    slider.innerHTML = "";
+
+    canais
+    .slice(0,20)
+    .forEach(canal=>{
+
+        slider.innerHTML += `
+
+        <div class="canal-card"
+            onclick="abrirModal('${canal.id}')">
+
+            <div class="canal-logo">
+
+                <img
+                    src="${canal.logo_url}"
+                    alt="${canal.name}"
+                    loading="lazy">
+
+            </div>
+
+            <div class="canal-info">
+
+                <h3>${canal.name}</h3>
+
+                <p>
+
+                    <strong>Agora:</strong><br>
+
+                    ${canal.epg?.current?.title || "Programação indisponível"}
+
+                </p>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+    iniciarSliderCanais();
+
+}
