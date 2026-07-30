31017299e53539dbf01dfadab061c2b37323624f
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-30 10:58:37 -0300

    Create render.js

---
+++ js/render.js
@@ -0,0 +1,42 @@
+function montarPagina(canais){
+
+    const categorias = {};
+
+
+    canais.forEach(canal => {
+
+
+        if(!categorias[canal.category]){
+
+            categorias[canal.category] = [];
+
+        }
+
+
+        categorias[canal.category].push(canal);
+
+
+    });
+
+
+
+    const container = document.getElementById("categorias");
+
+
+    container.innerHTML = "";
+
+
+
+    Object.keys(categorias).forEach(nome => {
+
+
+        container.innerHTML += criarCategoria(
+            nome,
+            categorias[nome]
+        );
+
+
+    });
+
+
+}
