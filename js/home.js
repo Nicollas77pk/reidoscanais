e5f475eaf23324cad43d420d9c0744614526ce39
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-31 20:17:34 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,43 +1,35 @@
 document.addEventListener("DOMContentLoaded", iniciar);
 
 
-
 async function iniciar(){
 
-
     try{
-
 
         const resposta = await api("/channels");
 
 
         if(!resposta || !resposta.success){
 
-            console.error("Erro canais");
+            console.error("Erro ao carregar canais");
 
             return;
 
         }
 
 
-
         STORE.canais = resposta.data;
-
 
 
         carregarAgenda();
 
 
-
         carregarCategorias();
-
 
 
     }catch(erro){
 
-        console.error(erro);
+        console.error("Erro:", erro);
 
     }
-
 
 }
