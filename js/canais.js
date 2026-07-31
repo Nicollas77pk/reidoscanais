abe104536ccf38b53ab7ec149694538859028420
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-31 19:05:11 -0300

    Update canais.js

--- js/canais.js
+++ js/canais.js
@@ -47,3 +47,122 @@ function montarCanaisDestaque(canais){
     iniciarSliderCanais();
 
 }
+
+function iniciarSliderCanais(){
+
+    const slider =
+    document.getElementById("canaisSlider");
+
+    if(!slider) return;
+
+    const card =
+    slider.querySelector(".canal-card");
+
+    if(!card) return;
+
+    const largura =
+    card.offsetWidth + 16;
+
+    document
+    .getElementById("canaisNext")
+    .onclick = ()=>{
+
+        slider.scrollBy({
+
+            left: largura,
+
+            behavior:"smooth"
+
+        });
+
+    };
+
+    document
+    .getElementById("canaisPrev")
+    .onclick = ()=>{
+
+        slider.scrollBy({
+
+            left: -largura,
+
+            behavior:"smooth"
+
+        });
+
+    };
+
+    let intervalo =
+    setInterval(()=>{
+
+        if(
+
+            slider.scrollLeft + slider.clientWidth >=
+            slider.scrollWidth - 20
+
+        ){
+
+            slider.scrollTo({
+
+                left:0,
+
+                behavior:"smooth"
+
+            });
+
+        }else{
+
+            slider.scrollBy({
+
+                left:largura,
+
+                behavior:"smooth"
+
+            });
+
+        }
+
+    },6000);
+
+    slider.addEventListener("mouseenter",()=>{
+
+        clearInterval(intervalo);
+
+    });
+
+    slider.addEventListener("mouseleave",()=>{
+
+        intervalo =
+        setInterval(()=>{
+
+            if(
+
+                slider.scrollLeft + slider.clientWidth >=
+                slider.scrollWidth - 20
+
+            ){
+
+                slider.scrollTo({
+
+                    left:0,
+
+                    behavior:"smooth"
+
+                });
+
+            }else{
+
+                slider.scrollBy({
+
+                    left:largura,
+
+                    behavior:"smooth"
+
+                });
+
+            }
+
+        },6000);
+
+    });
+
+}
