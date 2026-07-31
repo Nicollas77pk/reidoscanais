9eb69c8f38b5279d1ef8a3c01f9e83f00e2c40d6
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-31 19:43:01 -0300

    Update categorias.js

--- js/categorias.js
+++ js/categorias.js
@@ -146,3 +146,56 @@ function criarSecaoCategoria(nome,canais){
     montarCategoria(id,canais);
 
 }
+
+function montarCategoria(id,canais){
+
+    const slider =
+    document.getElementById(id+"Slider");
+
+    if(!slider) return;
+
+    slider.innerHTML="";
+
+    canais.forEach(canal=>{
+
+        slider.innerHTML += `
+
+<div
+class="canal-card"
+onclick="abrirModal('${canal.id}')">
+
+    <div class="canal-logo">
+
+        <img
+        src="${canal.logo_url}"
+        alt="${canal.name}">
+
+    </div>
+
+    <div class="canal-info">
+
+        <h3>
+
+            ${canal.name}
+
+        </h3>
+
+        <p>
+
+            <strong>Agora:</strong><br>
+
+            ${canal.epg?.current?.title || "Programação indisponível"}
+
+        </p>
+
+    </div>
+
+</div>
+
+`;
+
+    });
+
+    iniciarSliderCategoria(id);
+
+}
